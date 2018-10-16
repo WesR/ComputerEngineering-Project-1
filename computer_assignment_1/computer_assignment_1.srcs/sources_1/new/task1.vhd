d651d8a72c6d9ae8cc20c150fd79b01183ac8b04
wes <wring436@gmail.com>
2018-10-15 23:45:01 -0400

    defined all for task1

---
+++ computer_assignment_1/computer_assignment_1.srcs/sources_1/new/task1.vhd
@@ -0,0 +1,29 @@
+----------------------------------------------------------------------------------
+-- Engineer: Wes, Dylan, and Ron
+-- 
+-- Create Date: 10/15/2018 11:40:02 PM
+-- Design Name: task1
+-- Module Name: task1 - Behavioral
+--
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity task1 is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           cin : in STD_LOGIC);
+end task1;
+
+architecture Behavioral of task1 is
+
+begin
+
+
+end Behavioral;
