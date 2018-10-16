13978a29c5c25fd8c45ebd6b3aabb08c4b540d9c
wes <wring436@gmail.com>
2018-10-16 18:39:23 -0400

    Start of task2

---
+++ computer_assignment_1/computer_assignment_1.srcs/sources_1/new/task2.vhd
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------
+-- Engineer: Wes, Dylan, and Ron
+-- 
+-- Create Date: 10/15/2018 11:40:02 PM
+-- Design Name: task2
+-- Module Name: task2 - Behavioral
+--
+-- Revision 0.01 - File Created
+-- Additional Comments:
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity task2 is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c : in STD_LOGIC;
+           d : in STD_LOGIC;
+           f : out STD_LOGIC);
+end task2;
+
+architecture Behavioral of task2 is
+
+begin
+
+
+end Behavioral;
